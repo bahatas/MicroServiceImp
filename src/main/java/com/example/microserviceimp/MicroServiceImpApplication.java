3c686ed778c5fb91b5c54244ab04643bd74ab927
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2021-12-23 23:37:57 +0000

    initial commit for microservice

---
+++ src/main/java/com/example/microserviceimp/MicroServiceImpApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviceimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceImpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceImpApplication.class, args);
+    }
+
+}
